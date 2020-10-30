7c8ded5fb4536bc18a80b6c1f606fb107e758c82
Lea Wilhelmer <leawilhelmer@gmail.com>
2020-10-30 11:35:52 -0700

    moved outside of Develop

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-var hour8 = document.querySelector("#hour8");
-var hour9 = document.querySelector("#hour9");
-var hour10 = document.querySelector("#hour10");
-var hour11 = document.querySelector("#hour11");
-var hour12 = document.querySelector("#hour12");
-var hour13 = document.querySelector("#hour13");
-var hour14 = document.querySelector("#hour14");
-var hour15 = document.querySelector("#hour15");
-var hour16 = document.querySelector("#hour16");
-var hour17 = document.querySelector("#hour17");
-var hour18 = document.querySelector("#hour18");
-var safe = document.querySelector("span");
-var message = document.querySelector("textarea")
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-renderDescription();
-function displayMessage(type, message) {
-  message.textContent = message;
-  message.setAttribute("class", type);
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+    $("#hour8.description").val(localStorage.getItem("hour8"));
+    $("#hour9.description").val(localStorage.getItem("hour9"));
+    $("#hour10.description").val(localStorage.getItem("hour10"));
+    $("#hour11.description").val(localStorage.getItem("hour11"));
+    $("#hour12.description").val(localStorage.getItem("hour12"));
+    $("#hour13.description").val(localStorage.getItem("hour13"));
+    $("#hour14.description").val(localStorage.getItem("hour14"));
+    $("#hour15.description").val(localStorage.getItem("hour15"));
+    $("#hour16.description").val(localStorage.getItem("hour16"));
+    $("#hour17.description").val(localStorage.getItem("hour17"));
+    $("#hour18.description").val(localStorage.getItem("hour18"));
+
+function timeCheck() {
+    
+    if (timeCheck === null) {
+        window.localStorage.setItem(time, "")
+      } else if (timeCheck.length > 0) {
+        $(`#${time}`).attr("value", window.localStorage.getItem(time))
 }
-
-function renderDescription() {
-    var hour8 = localStorage.getItem("hour8");
-    if (hour8) {
-      return;
-    }
-    safe.textContent = span;
-  }
-
-  signUpButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    var hour8 = document.querySelector("#hour8").value;
-    if (hour8 === "") {
-      localStorage.setItem("");
-      renderDescription();
-    }
-  });
+}
+})
